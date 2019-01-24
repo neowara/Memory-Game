a8931473fdf2c43e72672031beae139766f165de
neowara <32764514+neowara@users.noreply.github.com>
2019-01-24 19:35:58 +0100

    Cleaned the code and also made it so the elements will dissapear when they match, invisible elements are not detected anymore and cannot be compared to other cards either. Preparations for CSS

--- script/game.js
+++ script/game.js
@@ -4,41 +4,58 @@ window.onload = function() {
     storedDeck.forEach(function(card, index){
         let cards = document.createElement("div");
         cards.classList.add("cardContainer");
-        cards.id = index;
+        cards.classList.add(index);
+        cards.id = card.typeOfCard;
         content.appendChild(cards);
         var cardContent = `
-            <div class = "${card.value} ${card.typeOfCard} card">
-            <h4> ${card.typeOfCard} </h4>
+        &#x200b;
+            <div class = "flip-card">
+            <div class = "flip-card-inner">
+                <div class = "flip-card-front"></div>
+                <div class="flip-card-back"></div>
             </div>
         `;
-        document.getElementById(cards.id).innerHTML = cardContent;
+        cards.innerHTML = cardContent;
     });
 
-    var allCards = document.querySelectorAll('h4');
+    var allCards = document.querySelectorAll('#content');
+    var allDivs = document.querySelectorAll('.cardContainer');
     var firstValue= null;
     var firstElement = null;
     var cardsPaired = 0;
- 
 
-    for(var i = 0; i < allCards.length; i++) {
-        allCards[i].addEventListener('click', (e) => {
+
+ 
+    for(var i = 0; i < allCards[0].childNodes.length; i++) {
+
+        allCards[0].childNodes[i].addEventListener('click', (e) => {
             var target = e.target || e.srcElement;
+
             if (firstValue === null){
-                firstValue = target.innerHTML;
+
+                firstValue = target.id;
                 firstElement = target;
-            } else if (firstValue === target.innerHTML && target !== firstElement)  {
-                console.log("cards match!");
+  
+            } else if (firstValue === target.id && target !== firstElement)  {
+
                 firstValue = null;
-                firstElement.className = "invisible";
-                target.className = "invisible";
+                firstElement.classList.add("invisible");
+                target.classList.add("invisible");
                 cardsPaired += 2;
-            } else if (firstElement === target) {
+                console.log("cards match!");
+ 
+            } else if (firstElement === target && !target.classList.contains("invisible")) {
+
                 console.log("you can't click the same card twice");
                 firstElement = target;
+
             } else {
+
                 console.log("not the same");
                 firstValue = null;
-            } if (cardsPaired === allCards.length) {
+
+            } if (cardsPaired === allCards[0].childNodes.length) {
+
                 console.log("you win!");
             }
           });
